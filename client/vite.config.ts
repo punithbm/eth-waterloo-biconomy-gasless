9678b6286e56ede1575ce8d0f3c8ea02b7583057
Punith B M <punith@designstring.com>
2023-06-24 22:11:35 +0530

    fix: minor updates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,23 @@
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': {},
+    'process.env': process.env
   },
+  build: {
+		rollupOptions: {
+			plugins: [
+				inject({
+					process: "process"
+				})
+			]
+		}
+	},
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
@@ -15,7 +26,6 @@ export default defineConfig({
       },
       plugins: [
         NodeGlobalsPolyfillPlugin({
-          process: true,
           buffer: true,
         }),
       ],
